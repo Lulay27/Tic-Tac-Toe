19bf2ab8e01bffd5e1af7235de0446f4d9ba33ea
SteezyLeezy <ericle201727@gmail.com>
2022-05-18 15:50:57 -0700

    added event listener for board-container to display COOL if i  click on one of the squares

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
-let square;
-
 for (let i = 0; i < 9; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.className = "square";
     document.querySelector('.board-container').appendChild(squareDiv);
 } 
 
+const boardSelector = document.querySelector('.board-container');
+
+boardSelector.addEventListener('click',cross);
+
+function cross(e) {
+    if (e.target.className = 'square') {
+        e.target.innerHTML = 'COOL';
+    }
+    // alert(e.target);
+}
