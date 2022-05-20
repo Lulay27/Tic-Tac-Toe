fe496dfbbae367dddbe6b5ab90591ff329253829
SteezyLeezy <ericle201727@gmail.com>
2022-05-20 01:41:49 -0700

    added first module for the game board, still learning factory functions and oop.

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-function game() {
+// factory function
+const player = (name, turn) => {
 
-}
+    return {name, turn}
+};
 
-const gameBoard = (() => {
+// module
+const gameBoard = ((doc) => {       // add doc 
+    const boardArr = [];
 
-})();
+    const testingMethod = (name) => {
+        console.log(name + ' is my name');
+    }
 
+    const markMethod = (selector,message) => {    // marks point on array
+        doc.querySelector(selector).innerHTML = message;
+    }
+
+    return {
+        testingMethod,
+        markMethod
+    };  // must put document here and use 'doc' b/c markMethod
+    // uses DOM that grabs document from outside the module
+
+})(document);
+
+//module
 const displayController = (() => {
 
-})();
+
+})();   
+
+
+
+// tmr task change so div names arent index and set up array for that
+
+
 
 
 for (let i = 0; i < 9; i++) {
     const squareDiv = document.createElement('div');
-    squareDiv.className = "square";
+    squareDiv.className = i;
     document.querySelector('.board-container').appendChild(squareDiv);
 } 
 
@@ -22,8 +48,10 @@ const boardSelector = document.querySelector('.board-container');
 boardSelector.addEventListener('click',cross);
 
 function cross(e) {
-    if (e.target.className = 'square') {
-        e.target.innerHTML = 'COOL';
+    const index = e.target.className;
+    if (index.length == 1) {      // why when this was = instead of == made 4
+        gameBoard.markMethod(index,'X');
+        // e.target.innerHTML = 'please work';
     }
     // alert(e.target);
 }
